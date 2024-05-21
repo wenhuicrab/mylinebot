02460f6cea18a4d35669a299bccba5bd3c885d10
wenhuicrab <118326302+wenhuicrab@users.noreply.github.com>
2024-05-21 14:46:51 +0800

    新增檔案

---
+++ mylineapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+def index(request):
+    return HttpResponse("hello")
+
+
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            # 若有訊息事件
+            if isinstance(event, MessageEvent):
+                # 回傳收到的文字訊息
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=event.message.text))
+
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
