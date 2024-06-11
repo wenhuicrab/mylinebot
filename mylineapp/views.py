8bcd018ee1a9c7038320a63da61f5162c7bef2e6
wenhuicrab <w871811@gmail.com>
2024-06-11 15:28:53 +0800

    Update views.py

--- mylineapp/views.py
+++ mylineapp/views.py
@@ -8,6 +8,7 @@ from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
 from linebot.models import StickerSendMessage
 from linebot.models import ImageSendMessage
+from linebot.models import LocationSendMessage
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
