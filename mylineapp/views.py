dcb459341c0210bdac064fd2d4b8cafb747e2e36
wenhuicrab <118326302+wenhuicrab@users.noreply.github.com>
2024-06-18 15:47:47 +0800

    Update views.py

--- mylineapp/views.py
+++ mylineapp/views.py
@@ -165,9 +165,9 @@ def callback(request):
                         TextSendMessage(text=msg)
                     )
                 elif msg == '九九乘法表':
-                    start_quiz(event.reply_token)  # 修正此行
+                    start_quiz(event.reply_token, line_bot_api)  # 傳遞 line_bot_api 參數
                 elif waiting_for_answer:
-                    handle_answer(msg, event.reply_token)
+                    handle_answer(msg, event.reply_token, line_bot_api)  # 傳遞 line_bot_api 參數
                     
                 else:
                     tdnow = datetime.datetime.now()
