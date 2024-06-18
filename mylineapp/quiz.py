ebe9c064fd8cbb3251a555ef0519ad596daa7990
wenhuicrab <118326302+wenhuicrab@users.noreply.github.com>
2024-06-18 15:55:54 +0800

    Update quiz.py

--- mylineapp/quiz.py
+++ mylineapp/quiz.py
@@ -1,14 +1,16 @@
 import random
 from linebot.models import TextSendMessage
 
-def start_quiz(reply_token, line_bot_api):
-    global waiting_for_answer, current_question, correct_count
-    waiting_for_answer = True
-    correct_count = 0  # 初始化正確答題計數器
-    multiplication_quiz(reply_token, line_bot_api)
+current_question = None
+correct_count = 0
 
-def handle_answer(msg, reply_token, line_bot_api):
-    global waiting_for_answer, correct_count
+def start_quiz():
+    global current_question, correct_count
+    correct_count = 0
+    multiplication_quiz()
+
+def handle_answer(msg):
+    global correct_count
     
     try:
         user_answer = int(msg)
@@ -17,11 +19,9 @@ def handle_answer(msg, reply_token, line_bot_api):
         num1, num2, correct_answer = current_question
         
         if user_answer == correct_answer:
-            print("恭喜你答對了！")
-            correct_count += 1  # 正確答題計數器加一
-            
+            correct_count += 1
             if correct_count < 10:
-                multiplication_quiz(reply_token, line_bot_api)
+                multiplication_quiz()
             else:
                 end_quiz()
         else:
@@ -30,31 +30,19 @@ def handle_answer(msg, reply_token, line_bot_api):
     except ValueError:
         print("請輸入有效的數字！")
 
-def multiplication_quiz(reply_token, line_bot_api):
+def multiplication_quiz():
     global current_question
     
-    while correct_count < 10:
-        num1 = random.randint(1, 9)
-        num2 = random.randint(1, 9)
-        
-        correct_answer = num1 * num2
-        
-        current_question = (num1, num2, correct_answer)
-        
-        line_bot_api.reply_message(
-            reply_token,
-            TextSendMessage(text=f"{num1} * {num2} 是多少？")
-        )
-        
-        # 等待使用者回答，這裡示範等待使用者回答的方式
-        return
-
-    print("恭喜你成功答對十題，做得很好！")
+    num1 = random.randint(1, 9)
+    num2 = random.randint(1, 9)
+    
+    correct_answer = num1 * num2
+    
+    current_question = (num1, num2, correct_answer)
+    
+    print(f"{num1} * {num2} 是多少？")
 
 def end_quiz():
-    global waiting_for_answer, correct_count
-    waiting_for_answer = False
+    global correct_count
     correct_count = 0
     print("測驗結束")
-
-# 在這裡可以添加更多需要的功能或函式
